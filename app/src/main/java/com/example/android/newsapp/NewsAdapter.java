96f1edbcc991d4ffec7b4b3c4d1bcf7de37309d1
liwona <liwona@users.noreply.github.com>
2018-05-12 19:20:48 +0200

    initial commit

---
+++ app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -0,0 +1,55 @@
+package com.example.android.newsapp;
+
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context The current context. Used to inflate the layout file.
+     * @param news A List of Earthquakes objects to display in a list
+     */
+    public NewsAdapter(Activity context, ArrayList<News> news) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, news);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_list_item, parent, false);
+        }
+
+        // Get the {@link NewsAdapter} object located at this position in the list
+        News currentNews = getItem(position);
+
+        // Find the TextView with view ID section
+        TextView sectionView = (TextView) listItemView.findViewById(R.id.section);
+        String section = currentNews.getSection();
+        sectionView.setText(section);
+
+
+        return listItemView;
+    }
+}
+
