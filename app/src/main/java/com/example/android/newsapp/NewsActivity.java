52013251220a19934c760c1e7d963f5e198608ea
liwona <liwona@users.noreply.github.com>
2018-05-13 22:47:27 +0200

    handling no internet connection & no data added

--- app/src/main/java/com/example/android/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -73,84 +72,68 @@ public class NewsActivity extends AppCompatActivity implements LoaderCallbacks <
             }
         });
 
-//        // Get a reference to the ConnectivityManager to check state of network connectivity
-//        ConnectivityManager connMgr = (ConnectivityManager)
-//                getSystemService(Context.CONNECTIVITY_SERVICE);
-//
-//        // Get details on the currently active default data network
-//        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-//
-//        // If there is a network connection, fetch data
-//        if (networkInfo != null && networkInfo.isConnected()) {
-//            //Get a reference to the LoaderManager, in order to interact with loaders.
-//            LoaderManager loaderManager = getLoaderManager();
-//
-//            Log.v("Before initLoader","next step: initLoader");
-//
-//            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-//            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-//            // because this activity implements the LoaderCallbacks interface).
-//            loaderManager.initLoader(NEWS_LOADER_ID, null, this).forceLoad();
-//        } else {
-//            //Otherwise, display error
-//            // First, hide loading indicator so error message will be visible
-//            View loadingIndicator = findViewById(R.id.loading_spinner);
-//            loadingIndicator.setVisibility(View.GONE);
-//
-//            mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
-//
-//            // Update empty state with no connection error message
-//            mEmptyStateTextView.setText(R.string.no_internet_connection);
-//
-//            earthquakeListView.setEmptyView(mEmptyStateTextView);
-//        }
-//
-////        // Get a reference to the LoaderManager, in order to interact with loaders.
-////        LoaderManager loaderManager = getLoaderManager();
-////        Log.v("Before initLoader","next step: initLoader");
-////        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-////        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-////        // because this activity implements the LoaderCallbacks interface).
-////        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this).forceLoad();
-//
-//        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
-//        earthquakeListView.setEmptyView(mEmptyStateTextView);
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        LoaderManager loaderManager = getLoaderManager();
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        Log.v("Before initLoader","next step: initLoader");
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            //Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
 
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(NEWS_LOADER_ID, null, this).forceLoad();
+            Log.v("Before initLoader","next step: initLoader");
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this).forceLoad();
+        } else {
+            //Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.progress_bar);
+            loadingIndicator.setVisibility(View.GONE);
+
+            mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+
+            newsListView.setEmptyView(mEmptyStateTextView);
+        }
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
 
     }
 
     @Override
     public Loader<List<News>> onCreateLoader(int id, Bundle args) {
-        Log.v("In onCreareLoader","before returning list");
+        Log.v("In onCreateLoader","before returning list");
         // Create a new loader for the given URL
         return new NewsLoader(this, GUARDIAN_REQUEST_URL);
     }
 
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
-//        // Hide loading indicator because the data has been loaded
-//        View loadingIndicator = findViewById(R.id.loading_spinner);
-//        loadingIndicator.setVisibility(View.GONE);
-//
-//        // Set empty state text to display "No earthquakes found."
-//        mEmptyStateTextView.setText(R.string.empty_message);
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.progress_bar);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No earthquakes found."
+        mEmptyStateTextView.setText(R.string.empty_message);
 
         // Clear the adapter of previous earthquake data
         mAdapter.clear();
-        Log.v("In onLoadeFinished","after mAdapter is cleared");
+        Log.v("In onLoadFinished","after mAdapter is cleared");
         // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
         // data set. This will trigger the ListView to update.
         if (news != null && !news.isEmpty()) {
             mAdapter.addAll(news);
-            Log.v("In onLoadeFinihed","after list is added to Adapter");
+            Log.v("In onLoadFinihed","after list is added to Adapter");
         }
     }
 
